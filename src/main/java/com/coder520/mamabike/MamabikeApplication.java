7debb3925918cecf285231cbd816829050033d67
18729901506@163.com <18729901506@163.com>
2017-09-01 10:23:20 +0800

    Initial commit

---
+++ src/main/java/com/coder520/mamabike/MamabikeApplication.java
@@ -0,0 +1,12 @@
+package com.coder520.mamabike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MamabikeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MamabikeApplication.class, args);
+	}
+}
